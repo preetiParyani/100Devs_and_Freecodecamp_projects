13521eeb7329b253d848399e8b70c69463fa38a9
Preeti Paryani <preetiparyani29@gmail.com>
2023-08-19 21:48:36 +0530

    allmost completed the flex box task just the task to hide the half menu is remaining

--- technoduxx/flexPractice/js/main.js
+++ technoduxx/flexPractice/js/main.js
@@ -1,23 +1,21 @@
 const displayConfirmation = () => {
-  document.querySelector(".mainContainer").classList.add("blurr");
+  document.querySelector(".main_container").classList.add("blurr");
   document.querySelector(".popup_container").classList.remove("hidden");
 };
 
-document
-  .querySelector("#saveBtn")
-  .addEventListener("click", displayConfirmation);
+document.querySelector("#save").addEventListener("click", displayConfirmation);
 
 const Exit = () => {
-  document.querySelector(".mainContainer").classList.remove("blurr");
+  document.querySelector(".main_container").classList.remove("blurr");
   document.querySelector(".popup_container").classList.add("hidden");
 };
 
 document.querySelector("#close").addEventListener("click", Exit);
 
 const collapseMenu = () => {
-  document.querySelector(".sideBar").classList.toggle("hideMenu");
+  document.querySelector(".sidebar_container").classList.toggle("hideMenu");
 
-  document.querySelector("main").style.width = "100%";
+  document.querySelector(".content_container").style.width = "100%";
 };
 
 document.querySelector("#menu").addEventListener("click", collapseMenu);
