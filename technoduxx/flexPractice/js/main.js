03888a9968f2ac57aae0981c745aa7624c07b70c
Preeti Paryani <preetiparyani29@gmail.com>
2023-08-21 15:31:53 +0530

    done some changes in flexbox

--- technoduxx/flexPractice/js/main.js
+++ technoduxx/flexPractice/js/main.js
@@ -28,3 +28,17 @@ const collapseMenu = () => {
 };
 
 document.querySelector("#menu").addEventListener("click", collapseMenu);
+
+
+function validate(){
+  var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+  var name = document.getElementById('name').value;
+  if(!regName.test(name)){
+      alert('Please enter your full name (first & last name).');
+      document.getElementById('name').focus();
+      return false;
+  }else{
+      alert('Valid name given.');
+      return true;
+  }
+}
