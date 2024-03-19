ac48ac8c44e53b7882315731558eea123e5140c9
Preeti Paryani <preetiparyani29@gmail.com>
2024-03-19 13:58:22 +0530

    Worked on Tic-Tac-Toe a little more

---
+++ Restart/Tic-Tac-Toe/JS/Main.js
@@ -0,0 +1,19 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+let turnO = true;
+const winPatterns = [
+  [0, 1, 2],
+  [0, 4, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box got clicked");
+  });
+});
